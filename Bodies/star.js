00df6cadbfd1a785820b8481a7e595d23230bc7c
f8hardin <laf8hardin@gmail.com>
2025-08-15 16:32:44 -0500

    Scroll Logic + Clean Up
    On scroll, camera zooms in and out. Cleaned up extra animtion logic to prepare for gravity simulation.

--- Bodies/star.js
+++ Bodies/star.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import PhysicsBody from './physicsBody'
+import PhysicsBody from './physicsBody.js'
 
 export default class Star extends PhysicsBody {
     constructor({mass = 1, position = [0, 0, 0], material = new THREE.MeshStandardMaterial({ color: 0xffffff }), geometry = new THREE.SphereGeometry(1, 32, 16), pointLight = new THREE.PointLight("#ffffff", 100, 0), ambientLight = null}){
@@ -8,16 +8,11 @@ export default class Star extends PhysicsBody {
         this.pointLight = pointLight;
         this.pointLight.position.copy(this.position);
         this.ambientLight = ambientLight;
-
-        this.group = new THREE.Group();
-        this.group.add(this);
-        this.group.add(this.ambientLight);
-        this.group.add(pointLight);
-        this.group.position.copy(this.position);
+        this.add(this.pointLight);
+        this.add(this.ambientLight);
     }
 
     setPosition(position) {
-        this.group.position.set(...position);
         this.pointLight.position.set(...position);
         this.position.set(...position);
     }
--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 import * as THREE from 'three';
-import PhysicsBody from './Bodies/physicsBody'
+import PhysicsBody from './Bodies/physicsBody.js'
 import Star from './Bodies/star'
 
 let scene, camera, renderer, sun, planet;
-let mouseMult = .05;
-let mousePositionX = 0;
-let mousePositionY = 0;
-let scrollDelta = 0;
-let scrollOffset = .01;
+let scrollModifier = .01;
 
 function init() {
   scene = new THREE.Scene();
@@ -22,19 +18,14 @@ function init() {
 
   sun = new Star({position : [0, 0, 0],  geometry : new THREE.SphereGeometry(5, 32, 16), pointLight : new THREE.PointLight("#f2df07", 1000, 0),  material : new THREE.MeshStandardMaterial({color : "#f2df07"}), ambientLight : new THREE.AmbientLight(0xffffff, 1)});
 
-  planet = new PhysicsBody({position : [0, 0, 0],  geometry : new THREE.SphereGeometry(1, 32, 16),  material : new THREE.MeshStandardMaterial({color : "#00ff00"})});
+  planet = new PhysicsBody({position : [10, 0, 0],  geometry : new THREE.SphereGeometry(1, 32, 16),  material : new THREE.MeshStandardMaterial({color : "#2d7af7"})});
   scene.add(planet)
-  scene.add(sun.group);
+  scene.add(sun);
 
   resize();
 }
 
 function animate() {
-  let directionX = mousePositionX - planet.position.x;
-  let directionY = mousePositionY - planet.position.y;
-
-  planet.position.set(planet.position.x + (directionX * mouseMult), planet.position.y + (directionY * mouseMult), planet.position.z + scrollDelta);
-  scrollDelta = 0;
   renderer.render( scene, camera );
 }
 
@@ -48,28 +39,12 @@ function clamp(value, min, max) {
   return Math.max(min, Math.min(max, value));
 }
 
-window.addEventListener('mousemove', (e) => {
-  const mouse = new THREE.Vector2(
-    (e.clientX / window.innerWidth) * 2 - 1,
-    -(e.clientY / window.innerHeight) * 2 + 1
-  );
-
-  const raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera(mouse, camera);
-
-  const planeZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-  const intersection = new THREE.Vector3();
-  raycaster.ray.intersectPlane(planeZ, intersection);
-
-  mousePositionX = intersection.x;
-  mousePositionY = intersection.y;
-});
-
 window.addEventListener('wheel', (e) => {
-  scrollDelta += e.deltaY * scrollOffset;
-  scrollDelta = clamp(scrollDelta, -10, 10);
-  console.log(scrollDelta);
+  let scrollY = e.deltaY * scrollModifier;
+  camera.position.z = clamp(camera.position.z + scrollY, 10, 50);
 });
+
+//window.addEventListener('click', (e) => {});
 
 window.addEventListener( 'resize', resize );
 init()
