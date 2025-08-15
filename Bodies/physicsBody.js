7a12b062b3b76575357b548a842bb1023094167d
f8hardin <laf8hardin@gmail.com>
2025-08-15 18:52:59 -0500

    Simple Gravity Simulation Calculations
    Calculating the acceleration between each body and applying it, allowing planets to start moving towards each other.

--- Bodies/physicsBody.js
+++ Bodies/physicsBody.js
@@ -1,11 +1,23 @@
 import * as THREE from 'three';
+//using metric system
 
 export default class PhysicsBody extends THREE.Mesh {
-    constructor ({mass = 1, position = [0, 0, 0], material = new THREE.MeshStandardMaterial({ color: 0xffffff }), geometry = new THREE.SphereGeometry(1, 32, 16)}) {
+    constructor ({mass = 1, position = [0, 0, 0], initialVelocity = [0, 0, 0], material = new THREE.MeshStandardMaterial({ color: 0xffffff }), geometry = new THREE.SphereGeometry(1, 32, 16)}) {
         super(geometry, material)
         this.mass = mass;
         this.radius = geometry.parameters.radius;
         this.density = mass / ((4/3*Math.PI*((this.radius/2)**3)));
         this.position.set(...position);
+        this.velocity = initialVelocity;
+        this.acceleration = [0, 0, 0];
+    }
+
+    animate(timeSinceLastFrame){ //called in animate of main to update this physics body
+        this.position.x += (this.velocity[0] * timeSinceLastFrame);
+        this.position.y += (this.velocity[1] * timeSinceLastFrame);
+        this.position.z += (this.velocity[2] * timeSinceLastFrame);
+        this.velocity[0] += this.acceleration[0];
+        this.velocity[1] += this.acceleration[1];
+        this.velocity[2] += this.acceleration[2];
     }
 }
--- main.js
+++ main.js
@@ -1,31 +1,93 @@
 import * as THREE from 'three';
 import PhysicsBody from './Bodies/physicsBody.js'
 import Star from './Bodies/star'
+import { sqrt } from 'three/tsl';
 
-let scene, camera, renderer, sun, planet;
-let scrollModifier = .01;
+let scene, camera, renderer, pivot, sun, physBodies = [];
+let scrollModifier = .1;
+let spinCamera = false; let lastX = 0; let lastY = 0; let rotateModifier = .01; let cameraMin = 200; let cameraMax = 10000;
+let clock = new THREE.Clock();
+let gravConstant = .1;
 
 function init() {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.setAnimationLoop( animate );
   document.body.appendChild( renderer.domElement );
 
-  camera.position.z = 25;
+  camera.position.z = 400;
 
-  sun = new Star({position : [0, 0, 0],  geometry : new THREE.SphereGeometry(5, 32, 16), pointLight : new THREE.PointLight("#f2df07", 1000, 0),  material : new THREE.MeshStandardMaterial({color : "#f2df07"}), ambientLight : new THREE.AmbientLight(0xffffff, 1)});
+  pivot = new THREE.Group();
 
-  planet = new PhysicsBody({position : [10, 0, 0],  geometry : new THREE.SphereGeometry(1, 32, 16),  material : new THREE.MeshStandardMaterial({color : "#2d7af7"})});
-  scene.add(planet)
-  scene.add(sun);
+  sun = new Star({mass: 500, position : [0, 0, 0],  geometry : new THREE.SphereGeometry(20, 32, 16), pointLight : new THREE.PointLight("#f2df07", 1000, 0),  material : new THREE.MeshStandardMaterial({color : "#f2df07"}), ambientLight : new THREE.AmbientLight(0xffffff, 1)});
+  physBodies.push(sun);
+  
+  let planet1 = new PhysicsBody({mass: 20, position : [100, 10, 5], initialVelocity: [10, 15, -10], geometry : new THREE.SphereGeometry(10, 32, 16),  material : new THREE.MeshStandardMaterial({color : "#2d7af7"})});
+  physBodies.push(planet1);
+  let planet2 = new PhysicsBody({mass: 10, position : [200, 0, 0], initialVelocity: [5, 5, 5], geometry : new THREE.SphereGeometry(5, 32, 16),  material : new THREE.MeshStandardMaterial({color : "red"})});
+  physBodies.push(planet2);
+  let planet3 = new PhysicsBody({ mass: 20, position : [-300, 50, 0],  initialVelocity: [0, -6, 2],  geometry : new THREE.SphereGeometry(7, 32, 16),  material : new THREE.MeshStandardMaterial({ color : "blue" }) });
+  physBodies.push(planet3);
+  let planet4 = new PhysicsBody({ mass: 5, position : [0, -400, 0],  initialVelocity: [6, 0, -3],  geometry : new THREE.SphereGeometry(4, 32, 16),  material : new THREE.MeshStandardMaterial({ color : "green" }) });
+  physBodies.push(planet4);
+  let planet5 = new PhysicsBody({ mass: 15, position : [100, 300, 0],  initialVelocity: [-4, 2, 1],  geometry : new THREE.SphereGeometry(6, 32, 16),  material : new THREE.MeshStandardMaterial({ color : "purple" }) });
+  physBodies.push(planet5);
+  let planet6 = new PhysicsBody({ mass: 8, position : [0, 0, 500],  initialVelocity: [-3, -4, 0],  geometry : new THREE.SphereGeometry(3, 32, 16),  material : new THREE.MeshStandardMaterial({ color : "orange" }) });
+  physBodies.push(planet6);
+
+
+  for (let b of physBodies) {
+    pivot.add(b);
+  }
+  scene.add(pivot);
 
   resize();
 }
 
 function animate() {
+  let timeSinceLastFrame = clock.getDelta();
+
+  for (let b of physBodies) { //reset acceleration
+    b.acceleration[0] = 0;
+    b.acceleration[1] = 0;
+    b.acceleration[2] = 0;
+  }
+
+  for (let key in physBodies){
+    for (let otherBody in physBodies){
+      if (physBodies[key] != physBodies[otherBody]){ //dont simulate with itself
+        let m1 = physBodies[key];
+        let m2 = physBodies[otherBody];
+
+        let dx = m2.position.x - m1.position.x;
+        let dy = m2.position.y - m1.position.y;
+        let dz = m2.position.z - m1.position.z;
+
+        let distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+        let accelerationX = gravConstant * (m2.mass / (distance ** 3)) * dx;
+        let accelerationY = gravConstant * (m2.mass / (distance ** 3)) * dy;
+        let accelerationZ = gravConstant * (m2.mass / (distance ** 3)) * dz;
+
+        console.log("Acceleration. X: " + accelerationX + ". Y: " + accelerationY + ". Z: " + accelerationZ)
+        physBodies[key].acceleration[0] += accelerationX;
+        physBodies[key].acceleration[1] += accelerationY;
+        physBodies[key].acceleration[2] += accelerationZ;
+
+        physBodies[otherBody].acceleration[0] -= accelerationX;
+        physBodies[otherBody].acceleration[1] -= accelerationY;
+        physBodies[otherBody].acceleration[2] -= accelerationZ;
+      }
+    }
+  }
+
+  for (let b of physBodies){
+    b.animate(timeSinceLastFrame);
+  }
+
   renderer.render( scene, camera );
 }
 
@@ -41,10 +103,30 @@ function clamp(value, min, max) {
 
 window.addEventListener('wheel', (e) => {
   let scrollY = e.deltaY * scrollModifier;
-  camera.position.z = clamp(camera.position.z + scrollY, 10, 50);
+  camera.position.z = clamp(camera.position.z + scrollY, cameraMin, cameraMax);
 });
 
-//window.addEventListener('click', (e) => {});
+window.addEventListener('mousedown', (e) => { spinCamera = true; lastX = e.clientX; lastY = e.clientY});
+window.addEventListener('mouseup', (e) => { spinCamera = false;});
+
+window.addEventListener('mousemove', (e) => {
+  if (spinCamera) {
+    let deltaX = lastX - e.clientX;
+    let deltaY = lastY - e.clientY;
+
+    lastX = e.clientX;
+    lastY = e.clientY;
+
+    pivot.rotation.y += (deltaX * rotateModifier);
+    pivot.rotation.z += (deltaY * rotateModifier);
+  }
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'r'){
+    pivot.rotation.set(0, 0, 0);
+  }
+});
 
 window.addEventListener( 'resize', resize );
 init()
