fa67fc4de4c7e458ad6c0d452f4e4f7128bad99a
f8hardin <laf8hardin@gmail.com>
2025-09-28 10:58:55 -0500

    Increased Camera Fade Dist

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ export function init() {
   cleanUp();
   scene = new THREE.Scene();
   scene.fog = null;
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 10000 );
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 100000 );
   camera.position.z = cameraScroll;
 
   pivot = new THREE.Group();
